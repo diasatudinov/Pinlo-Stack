89b543d6d691b253788f2fde938141756c4625f2
Dias <dias.atudinov@gmail.com>
2025-06-16 18:36:18 +0500

    add main menu and splash screen

--- Pinlo Stack/Pinlo Stack/PSViews/PSMenuView/PSMenuView.swift
+++ Pinlo Stack/Pinlo Stack/PSViews/PSMenuView/PSMenuView.swift
@@ -2,15 +2,162 @@
 //  PSMenuView.swift
 //  Pinlo Stack
 //
-//  Created by Dias Atudinov on 16.06.2025.
 //
 
 import SwiftUI
 
 struct PSMenuView: View {
+    @State private var showGame = false
+    @State private var showAchievement = false
+    @State private var showStore = false
+    @State private var showSettings = false
+    @State private var showDailyTask = false
+
+    @AppStorage("dailyTaskRecieved1") private var dailyTaskRecieved = false
+    
+    @StateObject var achievementVM = SRAchievementsViewModel()
+    @StateObject var settingsVM = SettingsViewModelSR()
+//    @StateObject var shopVM = SRAbilitiesViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            VStack(spacing: 0) {
+                HStack(alignment: .top) {
+                    Button {
+                        showSettings = true
+                    } label: {
+                        Image(.settingsIconPS)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: SRDeviceInfo.shared.deviceType == .pad ? 130:65)
+                    }
+                    
+                    Spacer()
+                    
+                    CoinBgSR()
+                    
+                    
+                    
+                }.padding(.horizontal, 20)
+                
+                Spacer()
+                
+                VStack(spacing: 20) {
+                    Button {
+                        showGame = true
+                        
+                    } label: {
+                        
+                        ZStack {
+                            Image(.playIconPS)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SRDeviceInfo.shared.deviceType == .pad ? 200:146)
+                            
+                            
+                                
+                        }
+                    }
+                    
+                    
+                    Button {
+                        showStore = true
+                    } label: {
+                        ZStack {
+                            Image(.storeIconPS)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SRDeviceInfo.shared.deviceType == .pad ? 200:146)
+                            
+                            
+                                
+                        }
+                    }
+                    
+                    Button {
+                        showDailyTask.toggle()
+                    } label: {
+                        ZStack {
+                            Image(.dailyTaskIconPS)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SRDeviceInfo.shared.deviceType == .pad ? 200:146)
+                            
+                            
+                                
+                        }
+                    }
+                    
+                    Button {
+                        showAchievement = true
+                    } label: {
+                        ZStack {
+                            Image(.achievementIconPS)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SRDeviceInfo.shared.deviceType == .pad ? 200:146)
+                            
+                        }
+                    }
+                }
+                Spacer()
+            }
+            
+            if showDailyTask {
+                Color.black.opacity(0.6).ignoresSafeArea()
+                ZStack {
+                    
+                    Image(.dailyTaskBgPS)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    VStack {
+                        Spacer()
+                        
+                        Button {
+                            showDailyTask.toggle()
+                            if !dailyTaskRecieved {
+                                dailyTaskRecieved = true
+                            }
+                        } label: {
+                            Image(dailyTaskRecieved ? .recievedBtnPS : .getBtnPS)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SRDeviceInfo.shared.deviceType == .pad ? 150:100)
+                        }
+                    }.frame(height: SRDeviceInfo.shared.deviceType == .pad ? 750:450)
+                    
+                }.frame(height: SRDeviceInfo.shared.deviceType == .pad ? 700:400)
+            }
+            
+        }
+        .background(
+            ZStack {
+                Image(.appBgPS)
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+            }
+        )
+        .fullScreenCover(isPresented: $showGame) {
+//            SRGameLevelsView()
+        }
+        .fullScreenCover(isPresented: $showStore) {
+//            SRAbilitiesView(viewModel: shopVM)
+        }
+        .fullScreenCover(isPresented: $showAchievement) {
+            PSAchivementsView(viewModel: achievementVM)
+        }
+        .fullScreenCover(isPresented: $showSettings) {
+            PSSettingsView(settingsVM: settingsVM)
+        }
+        
+        
+        
+        
     }
+    
 }
 
 #Preview {
--- Pinlo Stack/Pinlo Stack/PSViews/PSMenuView/PSSplashScreen.swift
+++ Pinlo Stack/Pinlo Stack/PSViews/PSMenuView/PSSplashScreen.swift
@@ -2,14 +2,73 @@
 //  PSSplashScreen.swift
 //  Pinlo Stack
 //
-//  Created by Dias Atudinov on 16.06.2025.
 //
 
 import SwiftUI
 
 struct PSSplashScreen: View {
+    @State private var scale: CGFloat = 1.0
+    @State private var progress: CGFloat = 0.0
+    @State private var timer: Timer?
+    private var loaderWidth: CGFloat = {
+       return SRDeviceInfo.shared.deviceType == .pad ? 500:250
+    }()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(.loaderBgPS)
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                ZStack {
+                    Image(.loaderBallPS)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    
+                }
+                .frame(height: SRDeviceInfo.shared.deviceType == .pad ? 250:149)
+                
+                
+                Image(.loadingTextPS)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: SRDeviceInfo.shared.deviceType == .pad ? 120:64)
+                    .scaleEffect(scale)
+                    .animation(
+                        Animation.easeInOut(duration: 0.8)
+                            .repeatForever(autoreverses: true),
+                        value: scale
+                    )
+                    .onAppear {
+                        scale = 0.8
+                    }
+                    .padding(.vertical, 20)
+                
+               
+                
+                
+                Spacer()
+            }
+            
+            
+        }
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 100 {
+                progress += 1
+            } else {
+                timer.invalidate()
+            }
+        }
     }
 }
 
